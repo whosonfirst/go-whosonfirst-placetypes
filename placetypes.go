237ed1d653c6d99668d5249cb9da9d478f8b0d5e
thisisaaronland <aaron@mapzen.com>
2015-10-15 00:01:41 +0000

    stub out the actual interface for functions/methods following py-mz-wof-placetypes

--- placetypes.go
+++ placetypes.go
@@ -1,11 +1,70 @@
 package placetypes
 
+import (
+
+)
+
+type WOFPlacetypeName struct {
+     Lang string
+     Kind string
+     Name string
+}
+
 type WOFPlacetype struct {
      Id int
      Name string
+     Role string
      Parent []string
+}
+
+func NewPlacetypeName(name string) *WOFPlacetypeName, error {
+
+}
+
+func NewPlacetype(placetype string) *WOFPlacetype, error {
+
 }
 
 func IsValidPlacetype(placetype string) bool {
      return false
+}
+
+func Common() []string {
+     return WithRole("common")
+}
+
+func CommonOptional() []string {
+     return WithRole("common_optional")
+}
+
+func Optional() []string {
+     return WithRole("optional")
+}
+
+func WithRole(role string) []string {
+
+}
+
+func WithRoles(roles []string) []string {
+
+}
+
+func IsValidRole(role string) bool {
+
+}
+
+func (p WOFPlacetype) Names() []*WOFPlacetypeName {
+
+}
+
+func (p WOFPlacetype) Parents() []*WOFPlacetype {
+
+}
+
+func (p WOFPlacetype) Ancestors() []string {
+
+}
+
+func (p WOFPlacetype) AncestorsWithRoles([]string) []string {
+
 }
